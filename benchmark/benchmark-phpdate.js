6cbf10937bf314ce04bc876da210ae6adc2a67d2
Andras <andras@kinvey.com>
2017-05-06 19:24:40 -0400

    combine benchmarks into single file; 1.0.3

--- benchmark/benchmark-phpdate.js
+++ benchmark/benchmark-phpdate.js
@@ -1,9 +1,28 @@
 var phpdate = require('../index');
 
 var i, x, nloops = 1000000;
+var dt = new Date();
+
+var t1 = Date.now();
+for (i=0; i<nloops; i++) x = new Date(1e9 + i);
+var t2 = Date.now();
+console.log(x);
+console.log(nloops + " in ms", t2-t1);
 
 var t1 = Date.now();
 for (i=0; i<nloops; i++) x = phpdate('Y-m-d H:i:s');
+var t2 = Date.now();
+console.log(x);
+console.log(nloops + " in ms", t2-t1);
+
+var t1 = Date.now();
+for (i=0; i<nloops; i++) x = phpdate('Y-m-d H:i:s', new Date(1e9 + i));
+var t2 = Date.now();
+console.log(x);
+console.log(nloops + " in ms", t2-t1);
+
+var t1 = Date.now();
+for (i=0; i<nloops; i++) x = phpdate('Y-m-d H:i:s', undefined, {cacheResults: false});
 var t2 = Date.now();
 console.log(x);
 console.log(nloops + " in ms", t2-t1);
